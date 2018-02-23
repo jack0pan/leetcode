e305ce1b93c38b3fdd27307d384e76f221ee42cc
Jack <i@jackpan.me>
2018-02-23 11:06:13 +0800

    Update TwoSum.rb

--- Algorithm/TwoSum.rb
+++ Algorithm/TwoSum.rb
@@ -4,20 +4,10 @@
 def two_sum(nums, target)
     h = Hash.new
     nums.each_with_index do |num, index|
-        if h[num]
-            h[num].push(index)
-        else
-            h[num] = [index]
-        end
-    end
-    nums.each_with_index do |num, index|
         left = target - num
-        if h[left]
-            h[left].each do |left_index|
-                if left_index != index
-                    return [index, left_index]
-                end
-            end
+        if h[left] 
+            return [h[left], index]
         end
+        h[num] = index
     end
 end
