d6d0dff60fceb7750d6b4f8aba1c13f14e3951e5
Jack Pan <i@jackpan.me>
2018-04-28 09:46:17 +0800

    Update No. 94 Binary Tree Inorder Traversal.

--- algorithms/binary_tree_inorder_traversal/binary_tree_inorder_traversal.rb
+++ algorithms/binary_tree_inorder_traversal/binary_tree_inorder_traversal.rb
@@ -10,17 +10,16 @@
 # @param {TreeNode} root
 # @return {Integer[]}
 def inorder_traversal(root)
-  path = []
-  _traversal(path, root)
-  return path
-end
-
-def _traversal(path, node)
-  if node.nil?
-    return
-  else
-    _traversal(path, node.left)
-    path.push node.val
-    _traversal(path, node.right)
+  return [] if root.nil?
+  node, stack, path = root, [], []
+  while node or not stack.empty?
+    while node
+      stack.push node
+      node = node.left
+    end
+    node = stack.pop
+    path << node.val
+    node = node.right
   end
+  path
 end
