e474464dcaca9e421979733b005bb64bf80bfd3e
Jack Pan <i@jackpan.me>
2018-05-03 18:12:49 +0800

    Add No. 102 Binary Tree Level Order Traversal.

---
+++ algorithms/binary_tree_level_order_traversal/binary_tree_level_order_traversal.rb
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @return {Integer[][]}
+def level_order(root)
+  result, queue = [], []
+  return result if root.nil?
+  queue << root
+  while not queue.empty?
+    level = []
+    next_level = []
+    queue.each do |node|
+      level << node.val
+      next_level << node.left if node.left
+      next_level << node.right if node.right
+    end
+    result << level
+    queue = next_level
+  end
+  return result
+end
